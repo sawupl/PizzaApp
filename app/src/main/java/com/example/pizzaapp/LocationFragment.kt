deef415a2f3f124edaaf2e64bfbcbf61400f313f
vbifarik <99973240+vbifarik@users.noreply.github.com>
2023-12-04 21:36:05 +0400

    Ввод адреса рабочий, привязать к навигации

--- app/src/main/java/com/example/pizzaapp/LocationFragment.kt
+++ app/src/main/java/com/example/pizzaapp/LocationFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.pizzaapp.databinding.FragmentLocationBinding
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -15,7 +17,8 @@ import com.google.firebase.ktx.Firebase
 class LocationFragment : Fragment() {
     private lateinit var binding: FragmentLocationBinding
     private val db = Firebase.firestore
-    private var cities = mutableListOf<String>()
+    private val auth = Firebase.auth
+    private var streets = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +27,7 @@ class LocationFragment : Fragment() {
 
         refDb.orderBy("name").get().addOnSuccessListener {documents ->
             for (document in documents) {
-                cities.add(document.data.get("name").toString())
+                streets.add(document.data.get("name").toString())
             }
         }
 
@@ -36,15 +39,58 @@ class LocationFragment : Fragment() {
     ): View {
         binding  = FragmentLocationBinding.inflate(inflater, container, false)
 
-        binding.db.setOnClickListener {
-
-        }
 
         val adapter = ArrayAdapter(
             requireContext(),
-            R.layout.simple_dropdown_item_1line, cities
+            R.layout.simple_dropdown_item_1line, streets
         )
         binding.streetText.setAdapter(adapter)
+
+
+        binding.addAddress.setOnClickListener {
+            var canBeAdded = true
+
+            val city = "Самара"
+
+            val street = binding.streetText.text.toString()
+            var correctStreet = false
+
+            if (street in streets){
+                correctStreet = true
+            }
+
+            if (!correctStreet){
+                canBeAdded = false
+                val toast = Toast.makeText(requireContext(), "Введите улицу корректно", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+
+            val house = binding.houseText.text.toString()
+            if (house.isEmpty()){
+                canBeAdded = false
+                val toast = Toast.makeText(requireContext(), "Введите номер дома", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+
+            val apartment = binding.apartmentText.text.toString()
+            if (apartment.isEmpty()){
+                canBeAdded = false
+                val toast = Toast.makeText(requireContext(), "Введите номер квартиры", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+
+            if (canBeAdded){
+                val addressString = "г.$city ул.$street д.$house кв.$apartment"
+                val address = hashMapOf(
+                    "address" to addressString
+                )
+                db.collection("users")
+                    .document(auth.currentUser?.uid.toString())
+                    .set(address)
+            }
+
+        }
+
         return binding.root
     }
 }
