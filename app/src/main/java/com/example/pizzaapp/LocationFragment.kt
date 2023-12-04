e588736bbe2f1fbaf61f7e06ed03ea6032dbaa58
vbifarik <99973240+vbifarik@users.noreply.github.com>
2023-12-04 20:28:23 +0400

    Список улиц

--- app/src/main/java/com/example/pizzaapp/LocationFragment.kt
+++ app/src/main/java/com/example/pizzaapp/LocationFragment.kt
@@ -1,59 +1,50 @@
 package com.example.pizzaapp
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.example.pizzaapp.databinding.FragmentLocationBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LocationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LocationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentLocationBinding
+    private val db = Firebase.firestore
+    private var cities = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        val refDb = db.collection("streets")
+
+        refDb.orderBy("name").get().addOnSuccessListener {documents ->
+            for (document in documents) {
+                cities.add(document.data.get("name").toString())
+            }
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
-    }
+    ): View {
+        binding  = FragmentLocationBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LocationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LocationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding.db.setOnClickListener {
+
+        }
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.simple_dropdown_item_1line, cities
+        )
+        binding.streetText.setAdapter(adapter)
+        return binding.root
     }
 }
